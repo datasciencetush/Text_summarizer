adedcd2306310954dc913672b39d0cee08d43933
tushdatascience <tushdatascience@gmail.com>
2023-05-30 16:38:49 +0530

    Folder Structure

---
+++ template.py
@@ -0,0 +1,51 @@
+import os
+import logging
+from pathlib import Path
+
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] : %(message)s')
+
+project_name = 'Text_Summarizer'
+
+list_of_Files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/comman.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "reasearch/trials.py"
+]
+
+for filepath in list_of_Files:
+    filepath =  Path(filepath)
+    filedir, file = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f'Creating Directory : {filedir} for the file {file}')
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+
+        logging.info(f'Creating empty file {filepath}')
+
+    else:
+        logging.info(f'creating is already exists {filepath}')
+
+
+
+
